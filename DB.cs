5ae852e6363b276417e92dc6bc8a399b63fae131
hRs <arek.mackiewicz@gmail.com>
2015-03-24 12:07:51 +0100

    DB, statyczna funkcja pobierająca HoneyTypes

---
+++ DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace KlasyfikacjaMiodu
+{
+    /// <summary>
+    /// Author: Arek Mackiewicz<para/>
+    /// All project data and current state is kept as a program Context.
+    /// Every Context realted action as new project, loading new data etc. can by listened with events.
+    /// </summary>
+    class DB
+    {
+        public static List<HoneyType> GetAllHoneyTypesFromFile()
+        {
+            List<HoneyType> HoneyList = new List<HoneyType>();
+
+            string HoneyName = "";
+            string HoneyDescriptionName = "";
+            Color HoneyMarkerColor = Color.Empty;
+
+            using (StreamReader reader = new StreamReader("honeyTypes.txt"))
+            {
+                while (true)
+                {
+                    HoneyName = reader.ReadLine();
+                    if (HoneyName == null) break;
+                    HoneyDescriptionName = reader.ReadLine();
+                    HoneyMarkerColor = Color.FromName(reader.ReadLine());
+                    HoneyType tmp = new HoneyType(HoneyName, HoneyDescriptionName, HoneyMarkerColor);
+                    HoneyList.Add(tmp);
+                }
+            }
+            return HoneyList;
+        }
+    }
+}
