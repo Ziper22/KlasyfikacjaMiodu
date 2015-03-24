17a979f735a18eedb51367ebf6348d39c58396bf
hRs <arek.mackiewicz@gmail.com>
2015-03-24 19:24:11 +0100

    DB, statyczna metoda pobierająca typy miodu(pyłków)

---
+++ DB.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Drawing;
+
+namespace KlasyfikacjaMiodu
+{
+    class DB
+    {
+        public static List<HoneyType> GetAllHoneyTypesFromFile()
+        {
+            List<HoneyType> HoneyList = new List<HoneyType>();
+
+            string HoneyName = "";
+            string HoneyDescriptionName = "";
+            Color HoneyMarkerColor = Color.Empty;
+
+            using (StreamReader reader = new StreamReader("HoneyTypes.txt"))
+            {
+                while (true)
+                {
+                    HoneyName = reader.ReadLine();
+                    if (HoneyName == null) break;
+                    HoneyDescriptionName = reader.ReadLine();
+                    HoneyMarkerColor = Color.FromName(reader.ReadLine());
+
+                    HoneyList.Add(new HoneyType(HoneyName, HoneyDescriptionName, HoneyMarkerColor));
+                }
+            }
+            return HoneyList;
+        }
+    }
+}
