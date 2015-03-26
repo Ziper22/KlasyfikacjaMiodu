4d01c901396470eb81a6bdc2ca360e56a61ae08e
msdiv <wifiprincess@live.com>
2015-03-26 17:48:10 +0100

    AddEditWindow + zmiany PollenModule/SidePanel

--- SidePanel.Designer.cs
+++ SidePanel.Designer.cs
@@ -28,59 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.dodajToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edytujToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.usuńToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // button1
+            // menuStrip1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Dodaj";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.dodajToolStripMenuItem,
+            this.edytujToolStripMenuItem,
+            this.usuńToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(187, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // button2
+            // dodajToolStripMenuItem
             // 
-            this.button2.Location = new System.Drawing.Point(81, 0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Usuń";
-            this.button2.UseVisualStyleBackColor = true;
+            this.dodajToolStripMenuItem.Name = "dodajToolStripMenuItem";
+            this.dodajToolStripMenuItem.Size = new System.Drawing.Size(51, 20);
+            this.dodajToolStripMenuItem.Text = "Dodaj";
+            this.dodajToolStripMenuItem.Click += new System.EventHandler(this.dodajToolStripMenuItem_Click);
             // 
-            // button3
+            // edytujToolStripMenuItem
             // 
-            this.button3.Location = new System.Drawing.Point(162, 0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Edytuj";
-            this.button3.UseVisualStyleBackColor = true;
+            this.edytujToolStripMenuItem.Name = "edytujToolStripMenuItem";
+            this.edytujToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.edytujToolStripMenuItem.Text = "Edytuj";
+            this.edytujToolStripMenuItem.Click += new System.EventHandler(this.edytujToolStripMenuItem_Click);
+            // 
+            // usuńToolStripMenuItem
+            // 
+            this.usuńToolStripMenuItem.Name = "usuńToolStripMenuItem";
+            this.usuńToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.usuńToolStripMenuItem.Text = "Usuń";
+            this.usuńToolStripMenuItem.Click += new System.EventHandler(this.usuńToolStripMenuItem_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Margin = new System.Windows.Forms.Padding(1);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(187, 263);
+            this.panel1.TabIndex = 1;
             // 
             // SidePanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.AutoScroll = true;
-            this.ClientSize = new System.Drawing.Size(279, 290);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(187, 287);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "SidePanel";
             this.Text = "SidePanel";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem dodajToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edytujToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem usuńToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel panel1;
     }
 }
--- SidePanel.cs
+++ SidePanel.cs
@@ -11,159 +11,47 @@ using System.Windows.Forms;
 namespace KlasyfikacjaMiodu
 {
     /// <summary>
-    /// Authors: Marek Borski, Agata Hammermeister<para/>
+    /// Author: Agata Hammermeister<para/>
     /// </summary>
-
     public partial class SidePanel : Form
     {
-        private List<PollenModule> PollensList = new List<PollenModule>();
-        private List<TextBox> TextBoxesList = new List<TextBox>();
-        private List<Label> LabelsList = new List<Label>();
-
-        private TextBox textbox;
-        private Label label;
-        private Button accept, cancel, chooseColor;
-        private Panel panel;
-        private PictureBox marker;
-
-        private int y1 = 25, y2 = 25, colorCode;
-        private string name, description;
-
         public SidePanel()
         {
             InitializeComponent();
         }
 
+        private void dodajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //AddEditWindow addEditWindow = new AddEditWindow();
+            //addEditWindow.Show();
+            PollenModule pm = new PollenModule("Nowy Pyłek", Color.DarkMagenta);
+                //tylko do testów 
+            panel1.Controls.Add(pm);
+
+            PollenModule pm1 = new PollenModule("Kolejny Pyłek", Color.Magenta);
+            //tylko do testów    
+            panel1.Controls.Add(pm1);
+
+        }
+
+        private void edytujToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            //wybór pollenmodule do edycji
+            //pm - wybrany
+            PollenModule pm = new PollenModule(); //tylko do testów
+            pm.Edit();
+        }
+
+        private void usuńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ///???????????
+            PollenModule pm = new PollenModule(); //tylko do testów
+            panel1.Container.Remove(pm);
+        }
+
         private void button1_Click(object sender, EventArgs e)
         {
-            button1.Visible = false;
 
-            panel = new Panel();
-            this.Controls.Add(panel);
-            panel.Location = new Point(0, 25);
-            panel.Size = new System.Drawing.Size(290, 230);
-            panel.BringToFront();
-
-            GetInformationPanel("name:");
-            GetInformationPanel("description:");
-
-            label = new Label();
-            this.Controls.Add(label);
-            label.Location = new Point(0, y1);
-            label.Text = "color:";
-            label.BringToFront();
-            LabelsList.Add(label);
-
-            chooseColor = new Button();
-            this.Controls.Add(chooseColor);
-            chooseColor.Location = new Point(75, y1);
-            chooseColor.Size = new System.Drawing.Size(150, 23);
-            chooseColor.Text = "Wybierz kolor";
-            chooseColor.BringToFront();
-
-            chooseColor.Click += chooseColor_Click;
-
-            accept = new Button();
-            this.Controls.Add(accept);
-            accept.Location = new Point(80, 120);
-            accept.Text = "OK";
-            accept.BringToFront();
-
-            accept.Click += accept_Click;
-
-            cancel = new Button();
-            this.Controls.Add(cancel);
-            cancel.Location = new Point(0, 120);
-            cancel.Text = "Anuluj";
-            cancel.BringToFront();
-
-            cancel.Click += cancel_Click;
-
-            y1 = 25;
         }
-
-        void chooseColor_Click(object sender, EventArgs e) 
-        {
-            ColorDialog dlg = new ColorDialog();
-            if (dlg.ShowDialog() == DialogResult.OK)
-            {
-                colorCode = dlg.Color.ToArgb();
-                chooseColor.Text = "Wybrano";
-            }
-        }
-
-        private void cancel_Click(object sender, EventArgs e) 
-        {
-            Remove_GetInformationPanel();
-        }
-
-        void accept_Click(object sender, EventArgs e) 
-        {
-            name = TextBoxesList[0].Text;
-            description = TextBoxesList[1].Text;
-
-            Remove_GetInformationPanel();
-
-            NewPollen();
-        }
-
-        private void NewPollen()
-        {
-            PollensList.Add(new PollenModule(0, 0, new HoneyType(name, description, Color.FromArgb(colorCode))));
-
-            marker = new PictureBox();
-            this.Controls.Add(marker);
-            marker.Location = new Point(5, y2);
-            marker.Size = new System.Drawing.Size(30, 30);
-            marker.BackColor = Color.FromArgb(colorCode);
-
-            label = new Label();
-            this.Controls.Add(label);
-            label.Location = new Point(40, y2 + 10);
-            label.Text = name;
-
-            label = new Label();
-            this.Controls.Add(label);
-            label.Location = new Point(40, y2 + 20);
-            label.Text = description;
-
-            y2 += 35;
-        }
-
-        private void GetInformationPanel(string text) 
-        {
-            label = new Label();
-            this.Controls.Add(label);
-            label.Location = new Point(0, y1);
-            label.Text = text;
-            label.BringToFront();
-            LabelsList.Add(label);
-
-            textbox = new TextBox();
-            this.Controls.Add(textbox);
-            textbox.Location = new Point(100, y1);
-            textbox.BringToFront();
-            TextBoxesList.Add(textbox);
-
-            y1 += 30;
-        }
-
-        private void Remove_GetInformationPanel() 
-        {
-            this.Controls.Remove(panel);
-            this.Controls.Remove(accept);
-            this.Controls.Remove(cancel);
-            this.Controls.Remove(chooseColor);
-            for (int i = 0; i < 2; i++)
-            {
-                this.Controls.Remove(TextBoxesList[i]);
-                this.Controls.Remove(LabelsList[i]);
-            }
-            this.Controls.Remove(LabelsList[2]);
-            TextBoxesList.Clear();
-            LabelsList.Clear();
-
-            button1.Visible = true;
-        }      
     }
 }
