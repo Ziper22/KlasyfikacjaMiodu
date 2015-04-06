157ef6a32370fe5947a8e38003ed33a448af7601
msdiv <wifiprincess@live.com>
2015-04-06 14:42:59 +0200

    Revert "Podświetlanie, wyśrodkowywanie mainform+sidepanel"
    This reverts commit 8746ba833fe2c4c23fa9b18310ac52658bbbbcf2.

---
+++ SidePanel.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Tracing;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KlasyfikacjaMiodu
+{
+    /// <summary>
+    /// Author: Agata Hammermeister<para/>
+    /// </summary>
+    public partial class SidePanel : Form
+    {
+        private PollenModule chosenModule;
+//        private PollenModuleSelector pollenModuleSelector;
+        public SidePanel()
+        {
+            InitializeComponent();
+            MouseClick += PollenModule_Clicked;
+            PollenModule chosenModule = null;
+//            pollenModuleSelector = new PollenModuleSelector();
+                       
+           //FormClosing += new FormClosingEventHandler(SidePanel_FormClosing);
+        }
+
+        private void PollenModule_Clicked(object sender, EventArgs e)
+        {
+            chosenModule = sender as PollenModule;
+        }
+
+        private void dodajToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HoneyTypeEditWindow addEditWindow = new HoneyTypeEditWindow();
+            addEditWindow.OkButtonClicked += HoneyTypeEditWindow_Add;
+            addEditWindow.Show();
+        }
+
+        private void HoneyTypeEditWindow_Add(HoneyType honeyType)
+        {
+            PollenModule pollenModule = new PollenModule(honeyType);
+//            pollenModuleSelector.AddListeners(pollenModule);
+            panel1.Controls.Add(pollenModule);
+        }
+
+        private void edytujToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            HoneyTypeEditWindow addEditWindow = new HoneyTypeEditWindow(chosenModule.HoneyType);
+            addEditWindow.OkButtonClicked += HoneyTypeEditWindow_Edit;
+            addEditWindow.Show();
+        }
+
+        private void HoneyTypeEditWindow_Edit(HoneyType honeyType)
+        {
+            chosenModule.Edit(honeyType);
+        }
+        
+       
+        private void usuńToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DialogResult dialogResult = MessageBox.Show("Czy na pewno chcesz usunąć wybrany znacznik?", "Znacznik zostanie usunięty", MessageBoxButtons.YesNo);
+            if (dialogResult == DialogResult.Yes)
+            {
+                panel1.Container.Remove(chosenModule);
+            }
+            else if (dialogResult == DialogResult.No)
+            {
+            }
+        }
+
+        private void pionowaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.FlowDirection = FlowDirection.TopDown;
+        }
+
+        private void poziomaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            panel1.FlowDirection = FlowDirection.LeftToRight;
+        }
+
+        //private void SidePanel_FormClosing(object sender, EventArgs e)
+        //{
+        //    this.Hide();
+        //    e.Cancel = true; // this cancels the close event
+        //}
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            base.OnFormClosing(e);
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                Hide();
+            }
+        }
+    }
+}
