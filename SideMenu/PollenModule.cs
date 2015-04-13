d39d6cd14acf19517b879d28021d2aaca2d96257
msdiv <wifiprincess@live.com>
2015-04-13 22:13:23 +0200

    Revert "Aktualizacja znaczników po edycji miodu"
    This reverts commit e22168ead3200ebb852ff34b7950691deb87bf76.

--- SideMenu/PollenModule.cs
+++ SideMenu/PollenModule.cs
@@ -82,7 +82,7 @@ namespace KlasyfikacjaMiodu.SideMenu
             {
                 Number++;
             }
-            ComputeMarkerNumber();
+            Compute();
         }
 
         //zapisywanie
@@ -92,10 +92,10 @@ namespace KlasyfikacjaMiodu.SideMenu
             {
                 Number--;
             }
-            ComputeMarkerNumber();
+            Compute();
         }
 
-        private void ComputeMarkerNumber()
+        private void Compute() //zmienić nazwę na sensowniejszą :P
         {
             PollenNumber.Text = Number + " pyłków";
             int allMarkers = Session.Context.Markers.Count;
