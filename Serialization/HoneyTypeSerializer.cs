40a79bb29d5c0a32ca7d79fe964569b83f74f291
G00DD4Y <blueflowersarepretty@gmail.com>
2015-04-27 22:57:12 +0200

    Odczyt i zapis - Poprawki
    Kilka małych poprawek, które rzuciły mi się w oczy.

--- Serialization/HoneyTypeSerializer.cs
+++ Serialization/HoneyTypeSerializer.cs
@@ -22,9 +22,7 @@ namespace KlasyfikacjaMiodu.Serialization
                 honeyTypeString =
                     "NAME:" + honeyType.Name + " " +
                     "DESCRIPTION_NAME:" + honeyType.DescriptionName + " " +
-                    "LINKED_NAME:" + honeyType.LinkedName + " " +
                     "MARKER_COLOR:" + ToHexConverter(honeyType.MarkerColor) + " " +
-                    "MINIMAL_POLLENS_AMOUNT:" + honeyType.MinimalPollensAmount + " " +
                     "MINIMAL_POLLENS_PERCENTAGE_AMOUNT:" + honeyType.MinimalPollensPercentageAmount;
                 honeyTypeStringList.Add(honeyTypeString);
             }
@@ -37,9 +35,7 @@ namespace KlasyfikacjaMiodu.Serialization
 
             string name;
             string descriptionName;
-            string linkedName;
             Color markerColor;
-            float minimalPollensAmount;
             float minimalPollensPercentageAmount;
 
             // I'm getting a name of honey type
@@ -50,27 +46,15 @@ namespace KlasyfikacjaMiodu.Serialization
 
             // I'm getting a description name of honey type
             startIndex = line.IndexOf("DESCRIPTION_NAME:") + 17;
-            lastIndex = line.IndexOf(" LINKED_NAME");
+            lastIndex = line.IndexOf(" MARKER_COLOR");
             length = lastIndex - startIndex;
             descriptionName = line.Substring(startIndex, length);
 
-            // I'm getting a linked name of honey type
-            startIndex = line.IndexOf("LINKED_NAME:") + 12;
-            lastIndex = line.IndexOf(" MARKER_COLOR");
-            length = lastIndex - startIndex;
-            linkedName = line.Substring(startIndex, length);
-
             // I'm getting a marker color of honey type
             startIndex = line.IndexOf("MARKER_COLOR:") + 13;
-            lastIndex = line.IndexOf(" MINIMAL_POLLENS_AMOUNT");
-            length = lastIndex - startIndex;
-            markerColor = ToColorConverter(line.Substring(startIndex, length));
-
-            // I'm getting a minimal pollens amount;
-            startIndex = line.IndexOf("MINIMAL_POLLENS_AMOUNT:") + 23;
             lastIndex = line.IndexOf(" MINIMAL_POLLENS_P");
             length = lastIndex - startIndex;
-            minimalPollensAmount = float.Parse(line.Substring(startIndex, length));
+            markerColor = ToColorConverter(line.Substring(startIndex, length));
 
             // I'm getting a minimal pollens percentage amount;
             startIndex = line.IndexOf("PERCENTAGE_AMOUNT:") + 18;
@@ -79,7 +63,7 @@ namespace KlasyfikacjaMiodu.Serialization
             string s = line.Substring(startIndex, length);
             minimalPollensPercentageAmount = float.Parse(line.Substring(startIndex, length));
 
-            return new HoneyType(name, descriptionName, linkedName, markerColor, minimalPollensAmount, minimalPollensPercentageAmount);
+            return new HoneyType(name,descriptionName, markerColor, minimalPollensPercentageAmount);
         }
 
         private static String ToHexConverter(System.Drawing.Color c)
